7425e338424035659e276280aa9f845d7dd19622
Arthur Miranda Gomes <arthur.mirandag@gmail.com>
2018-03-11 20:08:51 -0300

    update

--- index.js
+++ index.js
@@ -28,12 +28,15 @@ client.on('message', msg => {
     case 'prefix' :
       let newPrefix = msg.content.split(' ').slice(1, 2)[0]
       config.prefix = newPrefix
-      fs.writeFile('./config.json', JSON.stringify(config), (err) => console.error)
+      fs.writeFile('./config.json', JSON.stringify(config), (err) => console.log(err))
       break
     case 'math' :
       let op = args.join(' ')
-      waApi.getShort(op).then(data => msg.channel.send(data)).catch(err => msg.channel.send(
-        'Ow moçada, não vou saber responder isso agora, mas podem ficar tranquilos que vou pesquisar e trago a resposta na proxima aula!'))
+      waApi.getShort(op).then(data => msg.channel.send(data)).catch(err => {
+        console.log(err)
+        msg.channel.send(
+          'Ow moçada, não vou saber responder isso agora, mas podem ficar tranquilos que vou pesquisar e trago a resposta na proxima aula!')
+      })
       break
     case 'gif' :
       let busca = args.join(' ')
@@ -42,7 +45,7 @@ client.on('message', msg => {
         .then((response) => {
           msg.channel.send(response.data.url)
         })
-        .catch((err) => {})
+        .catch((err) => { console.log(err) })
       break
     case 'leal' :
       msg.channel.send('Yo, im Leal', {tts: true})
